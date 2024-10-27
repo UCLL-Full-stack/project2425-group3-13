1690b98951da9ea400ed54a2b9bd6a29f9c3424f
Timothy De Jaeger <timothy.dejaeger@student.ucll.be>
2024-10-27 22:55:29 +0100

    added validation for user

--- back-end/model/user.ts
+++ back-end/model/user.ts
@@ -64,23 +64,54 @@ export class User {
         password: string;
         id?: number;
     }) {
+        
         if (!user.nationalRegisterNumber) {
             throw new Error('National register number is required.');
+        } else if (!this.validateNRN(user.nationalRegisterNumber)) {
+            throw new Error('Nationalregisternumber is not correct.')
         }
+
         if (!user.name) {
             throw new Error('Name is required.');
         }
+
         if (!user.isAdministrator) {
             throw new Error('Administrator status is required.');
         }
+
         if (!user.phoneNumber) {
             throw new Error('Phone number is required.');
+        } else if (!this.validatePhone(user.phoneNumber)) {
+            throw new Error('Phone pattern is not valid.')
         }
+
         if (!user.email) {
             throw new Error('Email is required.');
+        } else if (!this.emailPattern(user.email)) {
+            throw new Error('Email pattern is not valid.')
         }
+
         if (!user.password) {
             throw new Error('Password is required.');
         }
+    }
+
+    validateNRN(nrn: string):boolean {
+        // Dagen en maand moet nog gecorrigeerd worden
+        const nrnPattern = /^([0-9]{2})\.([0][1-9]|[1][1_2])\.([0-2][0-9]|[3][01])\-([0-9]{3})\.([0-9]{2})$/;
+        
+        return nrnPattern.test(nrn);
+    }
+
+    validatePhone(phone: string): boolean {
+        const phonePattern = /^(?:(?:\+32|0)\s?)?(?:[1-9]{1}\d{1})(?:[\s.-]?\d{2,3}){3}$/
+
+        return phonePattern.test(phone);
+    }
+
+    emailPattern(email: string): boolean  {
+        const emailPattern = /^([a-zA-Z0-9])*\.?([a-zA-Z0-9])*\@([a-zA-Z])*\.([a-zA-Z])*$/
+
+        return emailPattern.test(email);
     }
 }
