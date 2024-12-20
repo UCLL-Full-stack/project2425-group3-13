5eaf3aa3916fdd6a447b851f2800762eee53b235
IVAN-JIANG <ivanjiang.@student.ucll.be>
2024-12-20 10:53:06 +0100

    react component testing

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-  };
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
